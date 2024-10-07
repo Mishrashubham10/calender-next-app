bc85bdb4ba0ea3fafe27078e2d6df520fe1b2dfc
shubham.mishra <shubham.mishra@igniculuss.com>
2024-10-07 19:50:22 +0530

    database configuration using drizzle and neondatabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-  const { userId } = auth();
-  if (userId != null) redirect('/events');
+  // const { userId } = auth();
+  // if (userId != null) redirect('/events');
 
   return (
     <div className="text-center container my-4 mx-auto">
